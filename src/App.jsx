be6fe5834bf688a4477483193af1cfa293d179bc
Christoffer <christoffer@christoffergylin.com>
2023-03-01 22:12:59 +0100

    Clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 import { useQuizzes } from './redux/quizzes';
 import Result from './modules/Result';
 import Quizz from './modules/Quizz';
