91afd982601177df7f0d421f0d6cd6bd37ba1fe5
Christoffer <christoffer@christoffergylin.com>
2023-03-10 21:06:02 +0100

    Fixed QuestionsList height

--- src/components/QuestionList.jsx
+++ src/components/QuestionList.jsx
@@ -8,7 +8,7 @@ const QuestionList = (props) => {
     const quizzes = useQuizzes();
 
     return (
-        <div className="flex flex-col border border-slate-400 bg-slate-200 rounded-lg h-[42rem] lg:h-[34rem] p-4">
+        <div className="flex flex-col border border-slate-400 bg-slate-200 rounded-lg h-[44.3rem] lg:h-[36.6rem] p-4">
             <h2 className="text-xl font-semibold ml-4">Questions</h2>
             <ul className="flex flex-col overflow-auto p-4 text-lg text-slate-700 h-full">
                 {quizzes.questions.map((question, index) => {
